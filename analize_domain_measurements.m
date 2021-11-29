0f499c2b414d782acc8ac67f8834ed79b7de207c
david-shem-tov <david.shem-tov@mail.huji.ac.il>
2021-11-29 17:29:26 +0200

    find the way to define the domains

--- analize_domain_measurements.m
+++ analize_domain_measurements.m
@@ -32,8 +32,8 @@ end
 function [data]= getImage(file)
     data.image = imread(file.folder+"/"+file.name);
     data.gray = rgb2gray(data.image);
-    data.histogram = histeq(double(data.gray)/255);
-    data.bolIm = data.histogram>0.5;
+    data.rescale = rescale(data.gray);
+    data.boolIm = data.rescale>0.4;
     data.fileName= file.name;
     data.idx = str2double(file.name(1:3));
     %change the sign of voltage
